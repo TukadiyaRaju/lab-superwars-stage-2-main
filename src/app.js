8dd323ff4c1be5158903586caf8736e705bad1ee
Raju Tukadiya <115335924+TukadiyaRaju@users.noreply.github.com>
2022-11-18 00:06:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,88 +1,104 @@
-const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
-];
+// Helps to mimic div creations
+let HTMLElements = {};
+document.getElementById = jasmine.createSpy('HTML Element').and.callFake(function (ID) {
+  if (!HTMLElements[ID]) {
+    var newElement = document.createElement('div');
+    HTMLElements[ID] = newElement;
+  }
+  return HTMLElements[ID];
+});
+TEST_PLAYERS = ["Hero"];
 
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-    for (let i=0;i<20;i++) {
-        detailedPlayers.push({
-            name:players[i],
-            strength:getRandomStrength(),
-            image:`images/super-${i+1}.png`,
-            type: i%2==0?"hero":"villain"
-        })
-    }
-    console.log(detailedPlayers)
-    return detailedPlayers;
-}
+describe("Creating players", function () {
+  it("should create a  valid JSON object \
+    from given array of players", function () {
+    let players = initPlayers(TEST_PLAYERS);
 
-// getting random strength
-const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
-    return Math.ceil(Math.random()*101);
-}
+    expect(typeof players[0]).toBe('object');
+  });
 
-const buildPlayers = (players, type) => {
-    let fragment = '';
+  it("should create equal number of players for \
+    given players name", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let expectedLength = TEST_PLAYERS.length;
 
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-    if (type=="hero") {
-        for (let i=0;i<20;i+=2) {
-            fragment+=`
-                    <div class="player">
-                    <img src="${players[i].image}">
-                    <div class="name">${players[i].name}</div>
-                    <div class="strength">${players[i].strength}</div>
-                    </div>
-                    `
-        }
-    }
-    else {
-        for (let i=1;i<20;i+=2) {
-            fragment+=`
-                    <div class="player">
-                    <img src="${players[i].image}">
-                    <div class="name">${players[i].name}</div>
-                    <div class="strength">${players[i].strength}</div>
-                    </div>
-                    `
-        }
-    }
-    return fragment;
-}
-// Display players in HTML
-const viewPlayers = (players) => {
+    expect(players.length).toBe(expectedLength);
+  });
 
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+  it("should create player with a valid name \
+    ", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let keys = Object.keys(players[0]);
+    let found = keys.findIndex(k => k == 'name');
 
-}
+    expect(found).toBeGreaterThan(-1);
+    expect(players[0].name).toBeInstanceOf(String);
+  });
 
-window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  it("should create player with a valid image url \
+    ", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let keys = Object.keys(players[0]);
+    let found = keys.findIndex(k => k == 'image');
+
+    expect(found).toBeGreaterThan(-1);
+    expect(players[0].image).toBeInstanceOf(String);
+    expect(players[0].image).toMatch(/images\/super-[0-9]+\.png/);
+  });
+
+  it("should create player with a valid numerical strength \
+    ", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let keys = Object.keys(players[0]);
+    let found = keys.findIndex(k => k == 'strength');
+
+    expect(found).toBeGreaterThan(-1);
+    expect(players[0].strength).toBeInstanceOf(Number);
+  });
+
+  it("should create player who is either Hero or Villain \
+    ", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let keys = Object.keys(players[0]);
+    let found = keys.findIndex(k => k == 'strength');
+
+    expect(found).toBeGreaterThan(-1);
+    expect(players[0].type).toMatch(/(hero|villain)/);
+  });
+});
+
+describe("Getting random strength", function () {
+  it("should return a number", function () {
+    let expectedType = Number;
+
+    let strength = getRandomStrength();
+
+    expect(strength).toBeInstanceOf(expectedType);
+  });
+
+  it("should return a 0 < number <= 100", function () {
+
+    let strength = getRandomStrength();
+
+    expect(strength).toBeGreaterThan(0);
+    expect(strength).toBeLessThanOrEqual(100);
+  });
+});
+
+describe("Building player blocks", function () {
+  it("should return a template for the players\
+    ", function () {
+    let players = initPlayers(TEST_PLAYERS);
+    let fragment = buildPlayers(players, 'hero');
+    fragment = fragment.replace(/\s/g, "");
+    let expectedFragment = `
+    <div class="player">
+        <img src="${players[0].image}" alt="">
+        <div class="name">${players[0].name}</div>
+        <div class="strength">${players[0].strength}</div>
+    </div>`;
+    expectedFragment = expectedFragment.replace(/\s/g, "");
+
+    expect(fragment).toBe(expectedFragment);
+  })
+});
